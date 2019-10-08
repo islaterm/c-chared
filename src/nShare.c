39f2a4877189fe2abc3083d7dcadebd2fca0435b
islaterm <ignacio.slater@ug.uchile.cl>
2019-10-08 09:32:06 -0300

    Base synchronization structure for nShare

--- src/nShare.c
+++ src/nShare.c
@@ -1,15 +1,39 @@
+/**
+ * Implementation of a concurrent sharing system using low level synchronization tools.
+ * 
+ * @author Ignacio Slater Muñoz
+ * @version 1.0b2
+ * @since 1.0
+ */
 #include "nSysimp.h"
 #include <nSystem.h>
+#include <stdarg.h>
 
-char *nRequest(nTask t, int timeout) {
-  nFatalError("nRequest not implemented", "%s");
+char *message;
+
+char *nRequest(nTask t, int timeout)
+{
+  nFatalError("nRequest", "%s\n", "Not implemented");
   return "";
 }
 
-void nRelease(nTask t){
+void nRelease(nTask t)
+{
   nFatalError("nRelease not implemented", "%s");
 }
 
-void nShare(char* data){
-  nFatalError("nShare not implemented", "%s");
+/**
+ * Ofrece datos para compartir.
+ * Cuando se llama a esta función se desbloquean todos los procesos que esperaban datos.
+ * 
+ * 
+ * @param data
+ *    los datos que serán compartidos
+*/
+void nShare(char *data)
+{
+  START_CRITICAL();
+  nPrintf("Entered critical section from nShare\n");
+  END_CRITICAL();
+  nPrintf("Exited critical section from nShare\n");
 }
