4a6d4355831daaf83e83d094b404f2790625147e
islaterm <ignacio.slater@ug.uchile.cl>
2019-10-11 19:44:25 -0300

    Implemented basic conditions for sharing v1.0b9

--- src/nShare.c
+++ src/nShare.c
@@ -2,7 +2,7 @@
  * Implementation of a concurrent sharing system using low level synchronization tools.
  * 
  * @author Ignacio Slater Muñoz
- * @version 1.0b8
+ * @version 1.0b9
  * @since 1.0
  */
 #include <stdarg.h>
@@ -41,12 +41,13 @@ char *preamble = "DEBUG   ";
 */
 char *nRequest(nTask t, int timeout)
 {
-  // char *data = NULL;
   nPrintf("%s[nRequest]   Pending requests: %d\n", preamble, pendingRequests);
   START_CRITICAL();
+
   nPrintf("%s[nRequest]   Entered critical section.\n", preamble);
   pendingRequests++;
   nPrintf("%s[nRequest]   Pending requests: %d\n", preamble, pendingRequests);
+
   PutTask(t->send_queue, current_task);
   nPrintf("%s[nRequest]   0x%X's timeout: %d\n", preamble, current_task, timeout);
   if (timeout > 0)
@@ -64,9 +65,14 @@ char *nRequest(nTask t, int timeout)
   }
   ResumeNextReadyTask();
   nPrintf("%s[nRequest]   Added 0x%X to 0x%X's send queue\n", preamble, current_task, t);
+  pendingRequests--;
+  nPrintf("%s[nRequest]   Pending requests: %d\n", preamble, pendingRequests);
+
   END_CRITICAL();
   nPrintf("%s[nRequest]   Exited critical section.\n", preamble);
-  return t->send.msg;
+  nPrintf("%s[nRequest]   0x%X received the following answer: %X.\n", preamble,
+          current_task, current_task->send.msg);
+  return current_task->send.msg;
 }
 
 void nRelease(nTask t)
@@ -85,15 +91,26 @@ void nShare(char *data)
 {
   START_CRITICAL();
   nPrintf("%s[nShare]   Entered critical section.\n", preamble);
-  isSharing = TRUE;
   nPrintf("%s[nShare]   0x%X started sharing %X\n", preamble, current_task, data);
   nPrintf("%s[nShare]   Looking for requests\n", preamble);
   while (!EmptyQueue(current_task->send_queue))
   {
     nTask requestingTask = GetTask(current_task->send_queue);
-    nPrintf("%s[nShare]   Answering request for task 0x%X\n", preamble, requestingTask);
+    if (requestingTask->status == WAIT_SHARE || requestingTask->status == WAIT_SHARE_TIMEOUT)
+    {
+      nPrintf("%s[nShare]   Answering request for task 0x%X\n", preamble, requestingTask);
+      if (requestingTask->status == WAIT_SHARE_TIMEOUT)
+      {
+        CancelTask(requestingTask);
+        nPrintf("%s[nShare]   Canceling task 0x%X because it reached it's timeout\n",
+                preamble, requestingTask);
+      }
+      requestingTask->status = READY;
+      PushTask(ready_queue, requestingTask);
+      nPrintf("%s[nShare]   Added 0x%X to the ready queue\n", preamble, requestingTask);
+    }
+    requestingTask->send.msg = data;
   }
-  isSharing = FALSE;
   nPrintf("%s[nShare]   0x%X finished sharing\n", preamble, current_task);
   END_CRITICAL();
   nPrintf("%s[nShare]   Exited critical section.\n", preamble);
